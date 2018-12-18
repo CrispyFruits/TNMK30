45f53533422e14cce74d2ca533e93ba85de63a91
Hugo Dahl <hugda958@student.liu.se>
2018-12-18 18:23:51 +0100

    semisemi-working my parts page

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,8 @@ function redirectMySets(){
     // navigate to new page
     window.location.href = 'index.php';
 }
+
+function redirectMyParts(){
+    // navigate to new page
+    window.location.href = 'my_parts.php';
+}
